0b48c9311e29da328bee394b4b955b9efeda48dc
YoonDii <dbswl9506@naver.com>
2022-10-27 17:50:49 +0900

    add comments

--- review/models.py
+++ review/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
 class Review(models.Model):
@@ -17,3 +19,13 @@ class Review(models.Model):
         format="JPEG",
         options={"quality": 80},
     )
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="like_review"
+    )
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
